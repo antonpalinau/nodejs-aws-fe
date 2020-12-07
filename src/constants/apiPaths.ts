9d73a624ea3e391d7759f8d219a893e4f7718aa3
antonpalinau <anton_palinau@epam.com>
2020-12-07 12:13:54 +0300

    added cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,6 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://cgprd0095g.execute-api.eu-west-1.amazonaws.com/dev/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://antonpalinau-cart-api-dev.eu-west-1.elasticbeanstalk.com//api",
 };
 
 export default API_PATHS;
